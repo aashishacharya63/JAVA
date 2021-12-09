2fc2c6f6cabca10113c228b05982adbb34e1f5ff
aashishacharya63 <75820832+aashishacharya63@users.noreply.github.com>
2021-12-09 21:39:50 +0545

    Add files via upload

---
+++ Lab/Lab-1/Question_number_1/parameterized_constructor.java
@@ -0,0 +1,20 @@
+class Pcon {
+
+  String fname;
+
+ 
+   Pcon(String name) {
+   name = "aashish";
+    fname = name;
+    System.out.println(fname + "aashish");
+    
+ 
+  }
+
+  public static void main(String[] args) {
+
+    
+    Pcon obj = new Pcon("aashish");
+
+  }
+}
